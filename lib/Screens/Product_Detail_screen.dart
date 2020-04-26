2db8208860f258fc7a5a3522afe30d6bc556695f
Kshitij Jain <jainkshitij99@gmail.com>
2020-04-27 00:54:41 +0530

    product detail added

--- lib/Screens/Product_Detail_screen.dart
+++ lib/Screens/Product_Detail_screen.dart
@@ -2,18 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/Models/Providers/Products_provider.dart';
 
-
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
-    final loadedProducts = Provider.of<Products>(context,listen: false)
-        .findById(productId);
+    final loadedProducts =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           loadedProducts.title,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: FittedBox(
+                child: Image.network(
+                  loadedProducts.imageUrl,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Rs ${loadedProducts.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 10,
+                ),
+                width: double.infinity,
+                child: Text(
+                  loadedProducts.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                )),
+          ],
         ),
       ),
     );
