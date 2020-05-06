71b3d37067700a372d099a45e6e698d01342b531
Kshitij Jain <jainkshitij99@gmail.com>
2020-05-07 03:26:49 +0530

    ik

--- lib/Models/Providers/auth.dart
+++ lib/Models/Providers/auth.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/Models/Providers/http_exception.dart';
+import 'package:shop/keys/apikey.dart' as api;
 
 class Auth with ChangeNotifier {
   String _token;
@@ -24,7 +25,7 @@ class Auth with ChangeNotifier {
   Future<void> authenticate(
       String email, String password, String segment) async {
     var url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$segment?key=AIzaSyCjg6Xz_MAk441ablASBTjEu_uOT1WgitE ';
+        'https://identitytoolkit.googleapis.com/v1/accounts:$segment?key=${api.key} ';
     try {
       final response = await http.post(url,
           body: json.encode({
